1ee16da67a2f51f4a168843eb690187aa88a717f
marathonic <castleracer@outlook.com>
2022-07-02 17:41:57 -0500

    can now play all cards and display a You Win message

--- src/App.js
+++ src/App.js
@@ -26,17 +26,7 @@ function App() {
           newDeck.push(cardObj);
           cardNames.push(cardName);
         }
-        //But what if all the cards shown on deck
-        //have been played before,
-        //while there's still unshown cards that didn't come up?
-        //The player would lose unfairly. Let's fix that.
-        //If new unshown cards, ensure at least 1 card on deck is new:
-
-        //Maybe we'd like to do: if(newDeck.length === 3 && newUnplayed.length > 0)
         if (newDeck.length === 3) {
-          //only run this if no new cards came out in the first 3 draws in the current deck,
-          //that way we avoid the edge case of e.g: having only 3 new cards, and them being drawn first,
-          //which would make it impossible to draw another new card, since we don't draw repeats.
           if (
             newDeck.every((card) => previouslyPlayed.includes(card.cardName))
           ) {
@@ -45,42 +35,12 @@ function App() {
             //get a new card that isn't in the array
             console.log("pulling a new card...");
 
-            //FOUND SOMETHING!!! We're serving the unplayedCards in the same order we're setting them in data.js
-            //So the order is always 1)Vraska, 2)Stoneforge Mystic, 3)Elspeth, etc...
-            //So of course the line below always outputs Vraska! That's why we always get Vraska as a "new" card!
-            //When we play a card, the other remaining unplayed cards jump back 1 step.
-            //So if we have 1)Vraska, 2)Stoneforge, 3) Elspeth,
-            //and we click Stoneforge, then we get 1)Vraska, 2)Elspeth, etc.
-            //We need to make it random every time
-
-            //FOUND SOMETHING ELSE: Even if we play Vraska and it stops showing up in the unplayed table we're
-            //logging to console, when the condition activates that logs: "3 OF THESE CARDS HAVE BEEN PLAYED BEFORE",
-            //"pulling a new card..." we always get back Vraska anyway, even if it's no longer in the unplayedCards array. Hmmm...
-
             console.log(unplayedCards[0]); //<--- this is just data.js, why?
-            //it seems that this function doesn't know that unplayedCards has updated from its initial state.
-            //This DOES NOT know the last card we've played, it doesn't update it right away, only after another turn, so 2 turns instead of 1.
-
-            //So App.js line 60 (the console log above) tells us the card we just picked, hasn't been clicked yet.
-            //Whereas App.js line 102 shows us the updated array, WITHOUT the card we've just clicked.
-            //I think it has to do with the dependencies array for the useEffect that runs updateUnplayedCards
             newDeck.push(unplayedCards[0][0]);
-            // newDeck.splice(2, 1, newlyMinted); <-- need a stateful array like previouslyPlayed(but that records nameOfCard)
-            // but recording the names of the images instead, so that we can check if that stateful array includes it,
-            // and if that stateful array doesn't include it, then that means that card hasn't been played it, so it's new!
-            // console.log("your new card would be: " + newlyMinted);
           }
         }
       }
       return newDeck;
-      // while (newDeck.length < 3) {
-      //   let randomNumber = Math.floor(Math.random() * data.length);
-      //   let cardObj = data[randomNumber];
-
-      //   if (!prevRandomDeck.includes(cardObj)) {
-      //     newDeck.push({ ...cardObj, prevRandomDeck });
-      //   }
-      // }
     });
   };
 
@@ -94,7 +54,15 @@ function App() {
         (obj) => previouslyPlayed.indexOf(obj.cardName) === -1
       );
 
-      const randomizedUnplayed = [];
+      if (
+        newUnplayed === undefined ||
+        newUnplayed === null ||
+        newUnplayed.length === 0
+      ) {
+        checkHighScore();
+        registerNewCard(false);
+        return ["FIRING UPDATE-UNPLAYED-CARDS AFTER SCORE IS MAX SCORE"];
+      }
 
       //Should we spread out newUnplayed?
       //We could return an object so that we don't have to [0][0].cardName in our getRandomDeck function
@@ -102,13 +70,6 @@ function App() {
       unplayedArr.push(newUnplayed);
       console.log("newUnplayed is: ");
       console.table(newUnplayed); //<-- This DOES know the last card we just played, it doesn't show it as unplayed.
-      // console.log("unplayedArr is: ");
-      // console.table(unplayedArr); //<-- This DOES know the last card we just played, it doesn't show it as unplayed.
-
-      //Because all previouslyPlayed cards come out at random, this array of unplayed cards will also be random.
-      //Since we update previouslyPlayed {card1, card2, card3, etc...} when we click on a card,
-      //that means that our newUnplayed array will be different each time its map function runs,
-      //so each time it runs it'll remove the previous card, since it will show up in previouslyPlayed
       return unplayedArr;
     });
   }
@@ -120,9 +81,9 @@ function App() {
     setPreviouslyPlayed((prevCards) => {
       const newArr = [];
 
-      if (previouslyPlayed.length === data.length) {
+      if (previouslyPlayed.length === data.length || score === data.length) {
         console.log("you win");
-        return;
+        newArr.push(...prevCards, "FIRING FROM PLAYCARD EVEN AT MAX SCORE");
       } else if (prevCards.includes(nameOfCard)) {
         checkHighScore();
         console.log("played the same card twice");
@@ -132,6 +93,7 @@ function App() {
         registerNewCard(true);
       }
       console.log(newArr);
+      //OK SO WE'RE RETURNING
       return newArr;
     });
   };
@@ -173,6 +135,11 @@ function App() {
       <ScoreBoard score={score} highScore={highScore} />
       {unplayedCards.length > 0 && (
         <div className="cards-section">{myPics}</div>
+      )}
+      {previouslyPlayed.length === data.length && (
+        <div className="winner">
+          <span>You win</span>
+        </div>
       )}
     </div>
   );
